5a7ce239db0a47e4d6b2d78f8a580ead60997dca
Majid Ali <h.majidali1515@gmail.com>
2025-09-20 17:43:34 +0400

    Food App delivery Header

---
+++ food-delivery-web/src/components/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='hidden-overflow h-[2000px]'>Home</div>
+  )
+}
+
+export default Home
---
+++ food-delivery-web/src/components/Home/Navbar/Nav.tsx
@@ -0,0 +1,65 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import { MdDeliveryDining } from 'react-icons/md'
+import { Navlinks } from '../../../../constant/constants'
+import Link from 'next/link'
+
+const Nav = () => {
+    const [navBg, setNaBag]=useState(false);
+    useEffect(()=>{
+        const handler =()=>{
+            if(window.scrollY>=90) setNaBag(true);
+            if(window.scrollY<90) setNaBag(false); 
+
+        };
+
+        window.addEventListener("scroll",handler);
+
+        return ()=> window.removeEventListener('scroll',handler);
+
+    },[]);
+
+  return (
+    <div className={`transition-all ${navBg? "bg-white shadow-md":"fixed"} duration-200 h-[12vh] z-[100] fixed w-full`}>
+        <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
+            {/* LOGO */}
+
+            <div className='flex items-center space-x-2'>
+                <div className='w-10 h-10 bg-blue-950  dark:bg:white  rounded-full flex items-center justify-center flex-col'>
+                    <MdDeliveryDining className='w-6 h-6 text-white'/>
+                </div>
+                <h1 className='text-xl hidden sm:block md:text-2xl text-black font-bold'>
+                    Foodie
+                </h1>
+            </div>
+
+            {/* Navlinks */}
+
+            <div className='hidden lg:flex items-center space-x-10'>
+                {Navlinks.map((link)=>{
+                    return (
+                          <Link
+                            key={link.id}
+                            href={link.url}
+                            className='text-black hover:text-green-700 font-bold transition-all duration-200'
+                            >
+                                <p>{link.label}</p>
+                            </Link>
+                    );              
+                })}
+            </div>
+            {/* buttons */}
+
+            <div className='flex items-center space-x-4'>
+                <button className='bg-blue-950 px-8 py-2.5 text-white font-bold  rounded-lg hover:bg-black 
+                transition-all duration-300 cursor-pointer '>
+                    Join Now
+                </button>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Nav
